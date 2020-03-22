2f34f4abf57248311340d119d61d56d389b1308c
Dennis <dkmullen@gmail.com>
2020-03-22 17:46:56 -0400

    Routing for offers

--- src/app/places/offers/offer-bookings/offer-bookings.page.ts
+++ src/app/places/offers/offer-bookings/offer-bookings.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { Place } from '../../places.model';
+import { ActivatedRoute } from '@angular/router';
+import { PlacesService } from '../../places.service';
 
 @Component({
   selector: 'app-offer-bookings',
@@ -7,10 +10,19 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./offer-bookings.page.scss'],
 })
 export class OfferBookingsPage implements OnInit {
+  place: Place;
 
-  constructor(private navCtrl: NavController) { }
+  constructor(private navCtrl: NavController, private activatedRoute: ActivatedRoute,
+    private placesService: PlacesService) { }
 
   ngOnInit() {
+    this.activatedRoute.paramMap.subscribe(paramMap => {
+      if (!paramMap.has('placeId')) {
+        this.navCtrl.navigateBack('/places/tabes/offers');
+        return;
+      }
+      this.place = this.placesService.getPlace(paramMap.get('placeId'));
+    });
   }
 
   onBookPlace() {
